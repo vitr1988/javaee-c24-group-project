c9d64f8351f63d87401ba607af31f8f73475997b
HaarDD <maxgalitskiy@gmail.com>
2024-02-27 23:36:46 +0300

    fix: playlist mapper

--- music-service/src/main/java/by/teachmeskills/musicservice/mapper/PlaylistMapper.java
+++ music-service/src/main/java/by/teachmeskills/musicservice/mapper/PlaylistMapper.java
@@ -2,8 +2,13 @@ package by.teachmeskills.musicservice.mapper;
 
 import by.teachmeskills.musicservice.dto.PlaylistDto;
 import by.teachmeskills.musicservice.entity.Playlist;
+import by.teachmeskills.musicservice.entity.Track;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(uses = TrackMapper.class)
 public interface PlaylistMapper {
@@ -12,6 +17,15 @@ public interface PlaylistMapper {
     PlaylistDto toPlaylistDto(Playlist playlist);
 
     @Mapping(target = "user.id", source = "userId")
-    @Mapping(target = "tracks", source = "trackIds")
+    @Mapping(target = "tracks", expression = "java(mapTrackIdsToTracks(playlistDto.trackIds()))")
     Playlist toPlaylist(PlaylistDto playlistDto);
+
+    default Set<Track> mapTrackIdsToTracks(List<Long> trackIds) {
+        return trackIds.stream().map(trackId -> {
+                    Track track = new Track();
+                    track.setId(trackId);
+                    return track;
+                })
+                .collect(Collectors.toSet());
+    }
 }
